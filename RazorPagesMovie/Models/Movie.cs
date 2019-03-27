ef157da9b2c1dd6604922ffa8235e31de034147b
Сергій Когут <sergio.kogut@gmail.com>
2019-03-27 23:50:45 +0200

    Add Filter and Search

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,11 +10,15 @@ namespace RazorPagesMovie.Models
     public class Movie
     {
         public int ID { get; set; }
+
         public string Title { get; set; }
 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
--- RazorPagesMovie/Program.cs
+++ RazorPagesMovie/Program.cs
@@ -5,8 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using RazorPagesMovie.Models;
 
 namespace RazorPagesMovie
 {
@@ -14,13 +17,36 @@ namespace RazorPagesMovie
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                
+                try
+                {
+                    var context = services.
+                        GetRequiredService<RazorPagesMovieContext>();
+                    context.Database.Migrate();
+                    SeedData.Initialize(services);
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogInformation("Information!");
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseKestrel()
-                .UseIISIntegration()
                 .UseStartup<Startup>();
     }
 }
+
+       
+
